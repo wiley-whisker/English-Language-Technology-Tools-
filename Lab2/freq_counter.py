4b2b614a70354bfbe42d0192ce844de02dd32787
wsm8855 <wsm8855@g.rit.edu>
2019-09-12 11:33:53 -0400

    Added freq_counter.

---
+++ Lab2/freq_counter.py
@@ -0,0 +1,56 @@
+"""
+Frequency counter for texts.
+
+author: Wiley Matthews
+"""
+
+import sys
+from typing import Dict
+import re
+
+TEXT_PATH = 'texts/'
+
+
+def read_file(filename : str) -> str:
+    text = ''
+    with open(filename) as f:
+        for line in f:
+            text += line
+    return text
+
+
+def clean(text : str) -> str:
+    text = re.sub(r'[.,]', '', text)
+    return text
+
+
+def freq(text : str) -> Dict[str, int]:
+    text = clean(text)
+    freq_dict = {}
+    word_list = text.split()
+    word_set = set(word_list)
+    for word in word_set:
+        freq_dict[word] = word_list.count(word)
+    return freq_dict
+
+
+def to_file(freqs : Dict[str, int], fname) -> None:
+    with open(fname, 'w') as f:
+        for word in freqs.keys():
+            f.write(word + " " + str(freqs[word]) + "\n")
+
+
+def main():
+    fname = sys.argv[1]
+    fpath = TEXT_PATH + fname
+    out_name = sys.argv[2]
+    out_path = TEXT_PATH + out_name
+
+    text = read_file(fpath)
+    freqs = freq(text)
+    to_file(freqs, out_path)
+
+
+
+if __name__ == '__main__':
+    main()
